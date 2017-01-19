2f9191c714860eb1f0c518267e41cab40bf8b6a5
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-18 21:58:52 -0500

    Initial Commit

---
+++ facebook_profile/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FacebookProfileConfig(AppConfig):
+    name = 'facebook_profile'
---
+++ facebook_profile/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render_to_response, redirect, render
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+@login_required(login_url='/')
+def home(request):
+    return render_to_response('home.html')
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
